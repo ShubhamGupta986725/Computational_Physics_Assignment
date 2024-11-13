ca1e5264205d81b236bc0b619e125d487b40b129
Shubham Gupta <f20210468@pilani.bits-pilani.ac.in>
2024-11-13 12:43:45 +0530

    Removed Comments

--- Assignment.c
+++ Assignment.c
@@ -184,19 +184,6 @@ void calculate(double simulation_time, double t_0, double* beta_array, double* l
 }
 
 
-/*
-   0 1 2 | 3 4 5 6 | 7 8 9 | 10 11
-
-   [0 2 6 9 11]
-
-   5
-
-
-   i -> beta[i], lambda[i]
-
-*/
-
-
 int main(void) {
    srand(time(NULL));
 }
